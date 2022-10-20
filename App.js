acce560e18a2a371b02ff69089554cb7fb9e61d0
ChristopherRdeKoker <christopherdekoker@gmail.com>
2022-10-20 20:14:43 +0200

    added framework, linked with android emu

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Ozmo Tracker</Text>
       <StatusBar style="auto" />
+      <Button title="Test button" />
     </View>
   );
 }
@@ -13,8 +14,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // backgroundColor: "#ADEC19",
+    backgroundColor: "#59d12b",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
