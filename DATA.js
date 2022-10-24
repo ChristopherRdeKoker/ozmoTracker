d15959136b8b4fe33922639cc31e20810ba8d083
ChristopherRdeKoker <christopherdekoker@gmail.com>
2022-10-24 14:16:34 +0200

    styled renderedCost List

--- DATA.js
+++ DATA.js
@@ -14,6 +14,34 @@ export const CostDATA = [
     quantity: 2,
     date: "2022-10-20",
   },
+  {
+    id: 4,
+    name: "Conditioner4",
+    costPrice: 250,
+    quantity: 1,
+    date: "2022-10-02",
+  },
+  {
+    id: 5,
+    name: "Mask",
+    costPrice: 75,
+    quantity: 6,
+    date: "2022-10-07",
+  },
+  {
+    id: 6,
+    name: "Juicy Mask",
+    costPrice: 79,
+    quantity: 4,
+    date: "2022-10-14",
+  },
+  {
+    id: 7,
+    name: "KittyMilk",
+    costPrice: 50,
+    quantity: 2,
+    date: "2022-10-18",
+  },
 ];
 
 export const SalesData = [
--- Screens/components/CostComponents/CostRenderedList.js
+++ Screens/components/CostComponents/CostRenderedList.js
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { View, Text, FlatList, StyleSheet } from "react-native";
-// import List from "./List";
 export default function CostRenderedList(props) {
   const [costs, setCosts] = useState(props.data);
 
@@ -15,7 +14,9 @@ export default function CostRenderedList(props) {
         data={costs}
         renderItem={({ item }) => (
           <View>
-            <Text style={styles.itemName}>{item.name}:</Text>
+            <Text style={styles.itemName}>
+              {item.id} : {item.name}
+            </Text>
             <Text style={styles.itemInfo}>
               R{item.costPrice} on {item.date}
             </Text>
@@ -30,11 +31,15 @@ const styles = StyleSheet.create({
   itemName: {
     backgroundColor: "#acdfdd",
     margin: 10,
-    fontSize: 28,
+    fontSize: 35,
+    padding: 2,
+    borderRadius: 10,
+    height: 50,
+    paddingLeft: 10,
   },
   itemInfo: {
     margin: 3,
-    fontSize: 20,
+    fontSize: 25,
     marginLeft: 20,
     marginBottom: 30,
   },
