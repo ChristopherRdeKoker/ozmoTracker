b17e13303c962300323289a13152dd20784549da
ChristopherRdeKoker <christopherdekoker@gmail.com>
2022-11-13 11:10:54 +0200

    Styling Inventory Section

--- Screens/InventoryScreen.js
+++ Screens/InventoryScreen.js
@@ -21,18 +21,6 @@ const styles = StyleSheet.create({
     justifyContent: "flex-start",
     flex: 1,
   },
-
-  renderedListContainer: {
-    flex: 1,
-    borderWidth: 4,
-    minHeight: 200,
-    width: "97%",
-    minWidth: "95%",
-    backgroundColor: "#89CFF0",
-    borderColor: "#28282B",
-    borderRadius: 10,
-    marginBottom: 20,
-  },
   header: {
     fontSize: 30,
     fontWeight: "bold",
--- Screens/components/InventoryComponents/InventoryTable.js
+++ Screens/components/InventoryComponents/InventoryTable.js
@@ -1,25 +1,26 @@
 import { StyleSheet, TouchableOpacity, Text, View, Modal } from "react-native";
 export default function InventoryTable() {
   return (
-    <View style={styles.origin}>
+    <View style={styles.main}>
       <View style={styles.table}></View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  origin: {
-    width: "90%",
+  main: {
+    width: "95%",
     marginLeft: "5%",
     marginRight: "5%",
 
-    // backgroundColor: "red",
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
   },
   table: {
     borderWidth: 3,
+    backgroundColor: "#FFD558",
+
     borderColor: "black",
     minHeight: 400,
     height: 550,
