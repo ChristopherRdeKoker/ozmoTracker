095f06d872f2d8dad49edf6d4cb368c7ce03027e
ChristopherRdeKoker <christopherdekoker@gmail.com>
2022-11-13 11:29:01 +0200

    styling inventory table

--- Screens/components/InventoryComponents/InventoryTable.js
+++ Screens/components/InventoryComponents/InventoryTable.js
@@ -1,8 +1,32 @@
-import { StyleSheet, TouchableOpacity, Text, View, Modal } from "react-native";
+import { StyleSheet, TouchableOpacity, Text, View, FlatList } from "react-native";
 export default function InventoryTable() {
+  // const data = ["Blinding Shine Shampoo", "Silverising Shampoo"];
+  const data = [
+    //temp data
+    { key: "Blinding Shine Shampoo", value: 2 },
+    { key: "Blinding Shine Conditioner", value: 5 },
+    { key: "Deep Moisturing Shampoo", value: 6 },
+    { key: "Deep Moisturing Conditioner", value: 1 },
+    { key: "Color Save Shampoo", value: 2 },
+    { key: "Color Save Conditioner", value: 4 },
+    { key: "Silverising Shampoo", value: 2 },
+    { key: "Intensive Deep Repair Mask", value: 1 },
+    { key: "Blinding Shine Definer", value: 7 },
+  ];
   return (
     <View style={styles.main}>
-      <View style={styles.table}></View>
+      <View style={styles.table}>
+        <FlatList
+          data={data}
+          renderItem={({ item }) => (
+            <View style={styles.row}>
+              <Text style={styles.text}>
+                {item.key} = {item.value}
+              </Text>
+            </View>
+          )}
+        />
+      </View>
     </View>
   );
 }
@@ -26,5 +50,19 @@ const styles = StyleSheet.create({
     height: 550,
     width: "95%",
     borderRadius: 10,
+  },
+  row: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    flexGrow: 1,
+    alignItems: "center",
+    width: "100%",
+    padding: 8,
+    marginTop: 15,
+  },
+  text: {
+    fontWeight: "bold",
+    fontSize: 20,
   },
 });
